4e70be795e22f50dcd00b9a27c5daec2cafa8eb1
Raid1542 <raidathallah036@gmail.com>
2025-01-05 10:38:20 +0700

    Delete AIS directory

--- AIS/includes/footer.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Pastikan tidak ada output sebelum kode PHP dimulai
-?>
-<!-- Footer content -->
-<footer>
-    <p>&copy; 2024 Astore. All Rights Reserved.</p>
-</footer>
--- AIS/menu_utama.js
+++
@@ -1,5 +0,0 @@
-const header = document.querySelector("header");
-
-window.addEventListener ("scroll",function(){
-    header.classList.toggl("sticky",this.window.scrollY > 0);
-})
